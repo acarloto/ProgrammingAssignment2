05906aec0227b6c0ce3fcbcee2b54165ed268040
António Carloto <acarloto@gmail.com>
2015-01-23 19:52:44 +0000

    Update cachematrix.R
    I tested my solution using a square invertible 3X3 matrix (A), assigned the inverted matrix computed to iA and then performed a matrix multiplication "round(A %*% iA)" that gave me the identity matrix (I) like it should.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This solution includes two functions that work in association to compute, 
+## in an efficient way, the inverse of a matrix.
 
-## Write a short comment describing this function
+## This first function receives a matrix and caches it, creating a special "matrix" 
+## wich is a list containing a function to a) set the matrix, 
+## b) get the matrix , c) set the inverse matrix and
+## d) get the inverse matrix.
+
 
 makeCacheMatrix <- function(x = matrix()) {
+	  i_matrix <- NULL
+      set <- function(y) {
+            x <<- y
+            i_matrix <<- NULL
+            
+      }
+      get <- function() x
+      setinverse <- function(solve) i_matrix <<- solve
+      getinverse <- function() i_matrix
+      list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## This second function computes the inverse of the special
+## matrix returned by the previous function above. If the inverse has
+## already been calculated (and the matrix has not changed), then
+## should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      i_matrix <- x$getinverse()
+      if(!is.null(i_matrix)) {
+            message("get cached data")
+            return(i_matrix)
+      }
+      data <- x$get()
+      i_matrix <- solve(data, ...)
+      x$setinverse(i_matrix)
+      i_matrix
 }
